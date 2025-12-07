2e93880a5f30ddf9d53ae579db9a3fa3ebff1780
huxiaoou <516984451@qq.com>
2025-12-07 08:59:03 +0800

    Update UIAvatar logic

--- scenes/ui/ui_avatar.gd
+++ scenes/ui/ui_avatar.gd
@@ -6,30 +6,43 @@ class_name UIAvatar
 @onready var bar_health_fg: TextureProgressBar = $TextureAvatar/BarHealthFg
 @onready var timer: Timer = $Timer
 
+
 func _ready() -> void:
 	bar_health_bg.value = bar_health_bg.max_value
 	bar_health_fg.value = bar_health_fg.max_value
 
+
+func _increase_health(health: float, duration_seconds: float = 0.5) -> void:
+	bar_health_fg.value = health
+	timer.start(0.3)
+	await timer.timeout
+	var tw: Tween = create_tween()
+	for _t in range(3):
+		tw.tween_property(bar_health_bg, "self_modulate:a", 0.5, 0.05)
+		tw.tween_property(bar_health_bg, "self_modulate:a", 1.0, 0.05)
+	tw.tween_property(bar_health_bg, "value", health, duration_seconds)
+	return
+
+
+func _decrease_health(health: float, duration_seconds: float = 0.5) -> void:
+	bar_health_bg.value = health
+	timer.start(0.3)
+	await timer.timeout
+	var tw: Tween = create_tween()
+	for _t in range(3):
+		tw.tween_property(bar_health_bg, "self_modulate:a", 0.5, 0.05)
+		tw.tween_property(bar_health_bg, "self_modulate:a", 1.0, 0.05)
+	tw.tween_property(bar_health_fg, "value", health, duration_seconds)
+	return
+
+
 func on_health_changed(health: float) -> void:
 	if health < bar_health_fg.value:
-		bar_health_fg.value = health
-		timer.start(0.3)
-		await timer.timeout
-		var tw: Tween = create_tween()
-		for _t in range(3):
-			tw.tween_property(bar_health_bg, "self_modulate:a", 0.5, 0.05)
-			tw.tween_property(bar_health_bg, "self_modulate:a", 1.0, 0.05)
-		tw.tween_property(bar_health_bg, "value", health, 0.5)
+		_increase_health(health)
 	elif health > bar_health_fg.value:
-		bar_health_bg.value = health
-		timer.start(0.3)
-		await timer.timeout
-		var tw: Tween = create_tween()
-		for _t in range(3):
-			tw.tween_property(bar_health_bg, "self_modulate:a", 0.5, 0.05)
-			tw.tween_property(bar_health_bg, "self_modulate:a", 1.0, 0.05)
-		tw.tween_property(bar_health_fg, "value", health, 0.5)
-	
+		_decrease_health(health)
+	return
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("test_add"):
@@ -38,3 +51,4 @@ func _input(event: InputEvent) -> void:
 	elif event.is_action_pressed("test_subtract"):
 		print("subtract health")
 		on_health_changed(clampf(bar_health_fg.value - 20, bar_health_fg.min_value, bar_health_fg.max_value))
+	return
