c9ad5def8ee025788a7018912d084b01684c27ce
huxiaoou <516984451@qq.com>
2025-12-10 23:47:50 +0800

    Use grid to auto detect boundary

--- scenes/levels/level_sidescroll.gd
+++ scenes/levels/level_sidescroll.gd
@@ -3,11 +3,11 @@ extends Node
 class_name LevelSidescroll
 
 @export_category("Side Scroll")
-@export var lim_left: int = 0
-@export var lim_right: int = 0
+@export var lim_left: float = 0
+@export var lim_right: float = 0
 
 @onready var camera_controller: CameraController = $CameraController
 
 
 func _ready() -> void:
-	camera_controller.set_camera_lim(Vector2i(lim_left, 0), Vector2i(lim_right, 0))
+	camera_controller.set_camera_lim(Vector2(lim_left, 0), Vector2(lim_right, 0))
--- scenes/levels/level_world.gd
+++ scenes/levels/level_world.gd
@@ -3,7 +3,13 @@ extends Node
 class_name LevelWorld
 
 @onready var camera_controller: CameraController = $CameraController
+@onready var layer_hex: LayerHex = $Map/LayerHex
 
 
 func _ready() -> void:
-	camera_controller.set_camera_lim(Vector2i(-1500, -1500), Vector2i(1500, 1500))
+	var lim_cells: Rect2i = layer_hex.get_used_rect()
+	print(lim_cells.position)
+	print(lim_cells.end)
+	var left_top_pos: Vector2i = layer_hex.grid_to_global_pos(lim_cells.position)
+	var right_bottom_pos: Vector2i = layer_hex.grid_to_global_pos(lim_cells.end)
+	camera_controller.set_camera_lim(left_top_pos + get_viewport().size / 2, right_bottom_pos - get_viewport().size /2)
