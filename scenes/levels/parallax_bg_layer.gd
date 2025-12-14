27675affe8f5eaa834120dfef01d86da80f9d133
huxiaoou <516984451@qq.com>
2025-12-14 21:10:03 +0800

    Add name ParallaxBgLayer when setting up

--- scenes/levels/parallax_bg_layer.gd
+++ scenes/levels/parallax_bg_layer.gd
@@ -5,7 +5,8 @@ class_name ParallaxBgLayer
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 
-func setup(texture: Texture2D, _scroll_scale: Vector2, _repeat_size: Vector2 = Vector2(1920, 0), _repeat_times: int = 5) -> void:
+func setup(id:int, texture: Texture2D, _scroll_scale: Vector2, _repeat_size: Vector2 = Vector2(1920, 0), _repeat_times: int = 5) -> void:
+	name = "ParaBgLayer%02d" % id
 	sprite_2d.texture = texture
 	scroll_scale = _scroll_scale
 	repeat_size = _repeat_size
