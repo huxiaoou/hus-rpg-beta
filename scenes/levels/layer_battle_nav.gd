683e561dad21e9e37bab6b3d072a6a645b999ed2
huxiaoou <516984451@qq.com>
2025-12-17 22:37:28 +0800

    Add ability move prototype

--- scenes/levels/layer_battle_nav.gd
+++ scenes/levels/layer_battle_nav.gd
@@ -2,6 +2,22 @@ extends TileMapLayer
 
 class_name LayerBattleNav
 
+var astar: AStarGrid2D = AStarGrid2D.new()
+
+
+func setup() -> void:
+	astar.region = get_used_rect()
+	astar.cell_size = tile_set.tile_size
+	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	astar.update()
+	return
+
+
+func get_grids_path(start_grid: Vector2i, end_grid: Vector2i) -> Array[Vector2i]:
+	if not astar.is_in_boundsv(end_grid):
+		return []
+	return astar.get_id_path(start_grid, end_grid, true)
+
 func get_mouse_grid() -> Vector2:
 	return global_pos_to_grid(get_global_mouse_position())
 
