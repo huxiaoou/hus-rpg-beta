f42c7823e5b2fbaaa5cad218f0e46cee535a7718
huxiaoou <516984451@qq.com>
2025-12-20 22:53:18 +0800

    Rename grids to cells in levelWorld

--- scenes/levels/layer_hex_world.gd
+++ scenes/levels/layer_hex_world.gd
@@ -3,61 +3,61 @@ extends TileMapLayer
 class_name LayerHexWorld
 
 enum { TILE_MODULATE_FULL, TILE_MODULATE_TRANS }
-var cur_grid: Vector2i = Vector2i(0, 0)
-var new_grid: Vector2i = Vector2i(0, 0)
+var cur_cell: Vector2i = Vector2i(0, 0)
+var new_cell: Vector2i = Vector2i(0, 0)
 
 
 func _process(_delta: float) -> void:
 	var mouse_global_pos: Vector2 = get_global_mouse_position()
-	new_grid = global_pos_to_grid(mouse_global_pos)
-	if new_grid != cur_grid:
-		update_lower_grids_alpha(cur_grid, TILE_MODULATE_FULL)
-		update_lower_grids_alpha(new_grid, TILE_MODULATE_TRANS)
-		cur_grid = new_grid
-		print(cur_grid)
+	new_cell = point_to_cell(mouse_global_pos)
+	if new_cell != cur_cell:
+		update_lower_cells_alpha(cur_cell, TILE_MODULATE_FULL)
+		update_lower_cells_alpha(new_cell, TILE_MODULATE_TRANS)
+		cur_cell = new_cell
+		print(cur_cell)
 
 
-func get_offset_left(grid: Vector2i, size: int = 1) -> Vector2i:
-	return grid + Vector2i(-1, 0) * size
+func get_offset_left(cell: Vector2i, size: int = 1) -> Vector2i:
+	return cell + Vector2i(-1, 0) * size
 
 
-func get_offset_right(grid: Vector2i, size: int = 1) -> Vector2i:
-	return grid + Vector2i(1, 0) * size
+func get_offset_right(cell: Vector2i, size: int = 1) -> Vector2i:
+	return cell + Vector2i(1, 0) * size
 
 
-func get_offset_left_upper(grid: Vector2i, size: int = 1) -> Vector2i:
-	return grid + (Vector2i(-1, -1) if grid.y % 2 == 0 else Vector2i(0, -1)) * size
+func get_offset_left_upper(cell: Vector2i, size: int = 1) -> Vector2i:
+	return cell + (Vector2i(-1, -1) if cell.y % 2 == 0 else Vector2i(0, -1)) * size
 
 
-func get_offset_left_lower(grid: Vector2i, size: int = 1) -> Vector2i:
-	return grid + (Vector2i(-1, 1) if grid.y % 2 == 0 else Vector2i(0, 1)) * size
+func get_offset_left_lower(cell: Vector2i, size: int = 1) -> Vector2i:
+	return cell + (Vector2i(-1, 1) if cell.y % 2 == 0 else Vector2i(0, 1)) * size
 
 
-func get_offset_right_upper(grid: Vector2i, size: int = 1) -> Vector2i:
-	return grid + (Vector2i(0, -1) if grid.y % 2 == 0 else Vector2i(1, -1)) * size
+func get_offset_right_upper(cell: Vector2i, size: int = 1) -> Vector2i:
+	return cell + (Vector2i(0, -1) if cell.y % 2 == 0 else Vector2i(1, -1)) * size
 
 
-func get_offset_right_lower(grid: Vector2i, size: int = 1) -> Vector2i:
-	return grid + (Vector2i(0, 1) if grid.y % 2 == 0 else Vector2i(1, 1)) * size
+func get_offset_right_lower(cell: Vector2i, size: int = 1) -> Vector2i:
+	return cell + (Vector2i(0, 1) if cell.y % 2 == 0 else Vector2i(1, 1)) * size
 
 
-func global_pos_to_grid(global_pos: Vector2) -> Vector2i:
-	return local_to_map(to_local(global_pos))
+func point_to_cell(point: Vector2) -> Vector2i:
+	return local_to_map(to_local(point))
 
 
-func grid_to_global_pos(grid: Vector2i) -> Vector2:
-	return to_global(map_to_local(grid))
+func cell_to_point(cell: Vector2i) -> Vector2:
+	return to_global(map_to_local(cell))
 
 
-func update_lower_grids_alpha(grid: Vector2i, alternative: int) -> void:
-	set_grid_alpha(get_offset_left_lower(grid), alternative)
-	set_grid_alpha(get_offset_right_lower(grid), alternative)
+func update_lower_cells_alpha(cell: Vector2i, alternative: int) -> void:
+	set_cell_alpha(get_offset_left_lower(cell), alternative)
+	set_cell_alpha(get_offset_right_lower(cell), alternative)
 	return
 
 
-func set_grid_alpha(grid: Vector2i, alternative: int) -> void:
-	var data: TileData = self.get_cell_tile_data(grid)
+func set_cell_alpha(cell: Vector2i, alternative: int) -> void:
+	var data: TileData = self.get_cell_tile_data(cell)
 	if data:
 		if data.get_custom_data("cross_hex"):
-			set_cell(grid, get_cell_source_id(grid), get_cell_atlas_coords(grid), alternative)
+			set_cell(cell, get_cell_source_id(cell), get_cell_atlas_coords(cell), alternative)
 	return
