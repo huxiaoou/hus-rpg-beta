ab7b0533bbf0f4176b206cebfb13dda0332aeeb9
huxiaoou <516984451@qq.com>
2025-12-21 21:24:33 +0800

    Add ability and ability_move

---
+++ scenes/abilities/ability.gd
@@ -0,0 +1,16 @@
+extends Node
+
+class_name Ability
+
+@export var short_name: String
+@export var id: String
+@export var description: String
+@export var icon: Texture2D
+
+var unit_owner: Unit = null
+var is_casting: bool = false
+
+
+func setup(_unit_owner: Unit) -> void:
+	unit_owner = _unit_owner
+	is_casting = false
