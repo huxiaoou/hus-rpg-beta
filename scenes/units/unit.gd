a66ed25c7862f689ac0cf8184ec43d8b4cec4091
huxiaoou <516984451@qq.com>
2025-12-23 22:19:46 +0800

    Refactor unit animation

--- scenes/units/unit.gd
+++ scenes/units/unit.gd
@@ -2,33 +2,31 @@ extends Node2D
 
 class_name Unit
 
-@export_group("walk")
-@export var walk_astream: AudioStream
-@export var walk_frames: Array[int]
+@export_group("Audios")
+@export var stream_walk: AudioStream
 
 @onready var character_body_2d: CharacterBody2D = $CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $CharacterBody2D/AnimatedSprite2D
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $CharacterBody2D/AudioStreamPlayer2D
 @onready var ability_move: AbilityMove = $AbilityMove
+@onready var animation_player: AnimationPlayer = $CharacterBody2D/AnimationPlayer
+
+var astreams: Dictionary[String, AudioStream] = {}
 
 
 func _ready() -> void:
     ability_move.unit_owner = self
-    animated_sprite_2d.frame_changed.connect(on_frame_changed)
+    astreams["walk"] = stream_walk
+    play_animation("idle")
     return
 
 
-func load_audio_stream(astream_to_load: AudioStream) -> void:
+func load_audio_stream(astream_name: String) -> void:
+    var astream_to_load: AudioStream = astreams[astream_name]
     if audio_stream_player_2d.stream != astream_to_load:
         audio_stream_player_2d.stop()
         audio_stream_player_2d.stream = astream_to_load
         return
-
-
-func on_frame_changed():
-    if animated_sprite_2d.animation == "walk":
-        if animated_sprite_2d.frame in walk_frames:
-            audio_stream_player_2d.play()
 
 
 func move_toward(target_pos: Vector2, distance: float) -> void:
@@ -55,5 +53,5 @@ func adjust_animation_direction(target_pos: Vector2) -> bool:
 
 
 func play_animation(animation: String) -> void:
-    animated_sprite_2d.play(animation)
+    animation_player.play(animation)
     return
