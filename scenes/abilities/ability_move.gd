49f335d8d97369fc545ea96bd1fa8776064d11af
huxiaoou <516984451@qq.com>
2025-12-23 22:56:22 +0800

    Fine tuned move speed and bg music

--- scenes/abilities/ability_move.gd
+++ scenes/abilities/ability_move.gd
@@ -2,7 +2,7 @@ extends Ability
 
 class_name AbilityMove
 
-@export var move_speed: float = 160
+@export var move_speed: float = 120
 
 var target_pos: Vector2 = Vector2(0, 0)
 var path_gp_points: Array[Vector2] = []
