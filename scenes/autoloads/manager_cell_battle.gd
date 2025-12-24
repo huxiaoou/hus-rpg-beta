bad00734fac83be994c4f39b0350c9f9e0a07ada
huxiaoou <516984451@qq.com>
2025-12-24 21:55:38 +0800

    Add node ManagerAbilities

--- scenes/autoloads/manager_cell_battle.gd
+++ scenes/autoloads/manager_cell_battle.gd
@@ -5,24 +5,24 @@ var cell_indicator_battle: CellIndicatorBattle
 
 
 func get_mouse_cell() -> Vector2i:
-	return layer_nav.get_mouse_cell()
+    return layer_nav.get_mouse_cell()
 
 
 func get_indicator_cell() -> Vector2i:
-	return cell_indicator_battle.indicator_cell
+    return cell_indicator_battle.indicator_cell
 
 
 func point_to_cell(global_pos: Vector2) -> Vector2i:
-	return layer_nav.point_to_cell(global_pos)
+    return layer_nav.point_to_cell(global_pos)
 
 
 func cell_to_point(cell: Vector2i) -> Vector2:
-	return layer_nav.cell_to_point(cell)
+    return layer_nav.cell_to_point(cell)
 
 
 func cell_is_walkable(cell: Vector2i) -> bool:
-	return layer_nav.cell_is_walkable(cell)
+    return layer_nav.cell_is_walkable(cell)
 
 
 func get_points_path(start_cell: Vector2i, end_cell: Vector2i) -> Array[Vector2]:
-	return layer_nav.get_points_path(start_cell, end_cell)
+    return layer_nav.get_points_path(start_cell, end_cell)
