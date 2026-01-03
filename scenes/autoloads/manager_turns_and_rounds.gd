82dc566b5471b814b79eb4f850276527bdf73a4f
huxiaoou <516984451@qq.com>
2026-01-03 21:27:58 +0800

    Refactor ManagerTurnsAndRounds

--- scenes/autoloads/manager_turns_and_rounds.gd
+++ scenes/autoloads/manager_turns_and_rounds.gd
@@ -3,7 +3,10 @@ extends Node
 signal turn_books_sorted()
 signal active_unit_changed()
 
-var active_unit: Unit = null
+var active_unit: Unit:
+    get:
+        return this_turn_book[0]
+
 var registered_units: Dictionary[int, Unit] = { }
 var this_turn_book: Array[Unit] = []
 var next_turn_book: Array[Unit] = []
@@ -14,7 +17,7 @@ func setup(units: Array[Unit]) -> void:
         register_unit(unit)
     sort_turn_books()
     if not this_turn_book.is_empty():
-        active_unit = this_turn_book[0]
+        active_unit.unit_turn_finished.connect(on_unit_turn_finished)
         active_unit_changed.emit()
     return
 
@@ -78,13 +81,17 @@ func refresh_turnbook() -> void:
     return
 
 
-func _unhandled_input(event: InputEvent) -> void:
-    if event.is_action_pressed("EndTurn"):
-        this_turn_book.pop_front()
-        print("Unit %s's turn ends." % active_unit.name)
-        if this_turn_book.is_empty():
-            refresh_turnbook()
-        active_unit = this_turn_book[0]
-        active_unit_changed.emit()
-        print_status()
+func on_unit_turn_finished(unit: Unit) -> void:
+    if unit != active_unit:
+        print("[WRN] wrong unit finishes turn")
+        return
+    active_unit.unit_turn_finished.disconnect(on_unit_turn_finished)
+    print("Unit %s's turn ends." % active_unit.name)
+    this_turn_book.pop_front()
+    if this_turn_book.is_empty():
+        refresh_turnbook()
+    active_unit.unit_turn_finished.connect(on_unit_turn_finished)
+    active_unit_changed.emit()
+    # print_status()
+    print("Unit %s' turn begins." % active_unit.name)
     return
