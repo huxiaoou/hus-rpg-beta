9f43b5bcad97088e9cd38b38e54a9b2cb51e41f9
huxiaoou <516984451@qq.com>
2026-01-04 16:43:44 +0800

    Fix a bug when deactivate ability move

--- scenes/levels/data_cell_battle.gd
+++ scenes/levels/data_cell_battle.gd
@@ -3,7 +3,5 @@ class_name DataCellBattle
 var walkable: bool = true
 var occupiant: Unit = null
 var is_occupied: bool:
-	get:
-		return occupiant != null
-	set(value):
-		is_occupied = value
+    get:
+        return occupiant != null
