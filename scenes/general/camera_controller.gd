d8f17d6e6ebb8fd29fda6244bd0f789d40f598ce
huxiaoou <516984451@qq.com>
2026-01-06 22:27:48 +0800

    Add offset to camera2d in camera_controller, and adjust everything relate to it

--- scenes/general/camera_controller.gd
+++ scenes/general/camera_controller.gd
@@ -2,7 +2,9 @@ extends Node
 
 class_name CameraController
 
-@export var camera_move_speed: float = 500
+@export_group("camera")
+@export var enable_move: bool = false
+@export var move_speed: float = 500
 
 @onready var camera_2d: Camera2D = $Camera2D
 
@@ -12,26 +14,31 @@ var target_position: Vector2
 
 
 func set_camera_lim(left_top: Vector2, right_down: Vector2) -> void:
-	lim_left_top = left_top
-	lim_right_down = right_down
-	return
+    lim_left_top = left_top
+    lim_right_down = right_down
+    return
 
 
 func _process(delta: float) -> void:
-	var camera_mov_direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if camera_mov_direction != Vector2.ZERO:
-		target_position = camera_2d.global_position + camera_mov_direction * camera_move_speed * delta
-	clamp_target_pos()
-	track_target_pos()
-	return
+    if enable_move:
+        var camera_mov_direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+        if camera_mov_direction != Vector2.ZERO:
+            target_position = camera_2d.global_position + camera_mov_direction * move_speed * delta
+        clamp_target_pos()
+        track_target_pos()
+    return
 
 
 func clamp_target_pos() -> void:
-	target_position.x = clamp(target_position.x, lim_left_top.x, lim_right_down.x)
-	target_position.y = clamp(target_position.y, lim_left_top.y, lim_right_down.y)
-	return
+    target_position.x = clamp(target_position.x, lim_left_top.x, lim_right_down.x)
+    target_position.y = clamp(target_position.y, lim_left_top.y, lim_right_down.y)
+    return
 
 
 func track_target_pos() -> void:
-	camera_2d.global_position = target_position
-	return
+    camera_2d.global_position = target_position
+    return
+
+
+func camera_offset() -> Vector2:
+    return camera_2d.offset
