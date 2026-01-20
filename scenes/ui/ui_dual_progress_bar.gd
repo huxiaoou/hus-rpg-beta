6de2146881aa5bdcd1a13d97b9a44dff38927ce4
huxiaoou <516984451@qq.com>
2026-01-20 23:36:52 +0800

    Add digits to show bar value for ui_dual_progress_bar

--- scenes/ui/ui_dual_progress_bar.gd
+++ scenes/ui/ui_dual_progress_bar.gd
@@ -16,6 +16,11 @@ class_name UIDualProgressBar
 @onready var pb_back: TextureProgressBar = $PBBack
 @onready var pb_fore: TextureProgressBar = $PBFore
 @onready var timer: Timer = $Timer
+@onready var label: Label = $Label
+
+
+func cal_pb_lbl_text(v: float, mv: float) -> String:
+    return "%d/%d" % [v, mv]
 
 
 func init_pb(pb: TextureProgressBar, pb_texture: Texture2D, pb_color: Color) -> void:
@@ -25,6 +30,8 @@ func init_pb(pb: TextureProgressBar, pb_texture: Texture2D, pb_color: Color) -> 
     pb.max_value = dual_pb_max_val
     pb.min_value = dual_pb_min_val
     pb.step = dual_pb_step
+    label.text = cal_pb_lbl_text(pb.value, pb.max_value)
+    label.custom_minimum_size.x = pb_fore.get_minimum_size().x * 0.9
     return
 
 
@@ -45,11 +52,18 @@ func _decrease_bar(
     pb_fore.value = new_val
     timer.start(delay_sceonds)
     await timer.timeout
-    var tw: Tween = create_tween()
+    var tw: Tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
     for t: int in range(blink_times):
         tw.tween_property(pb_back, "self_modulate:a", blink_attitude.x, blink_duration_seconds)
         tw.tween_property(pb_back, "self_modulate:a", blink_attitude.y, blink_duration_seconds)
     tw.tween_property(pb_back, "value", new_val, duration_seconds)
+    tw.parallel().tween_property(
+        label,
+        "text",
+        cal_pb_lbl_text(new_val, dual_pb_max_val),
+        duration_seconds,
+    )
+    dual_pb_val = new_val
     return
 
 
@@ -64,11 +78,18 @@ func _increase_bar(
     pb_back.value = new_val
     timer.start(delay_sceonds)
     await timer.timeout
-    var tw: Tween = create_tween()
+    var tw: Tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
     for t: int in range(blink_times):
         tw.tween_property(pb_back, "self_modulate:a", blink_attitude.x, blink_duration_seconds)
         tw.tween_property(pb_back, "self_modulate:a", blink_attitude.y, blink_duration_seconds)
     tw.tween_property(pb_fore, "value", new_val, duration_seconds)
+    tw.parallel().tween_property(
+        label,
+        "text",
+        cal_pb_lbl_text(new_val, dual_pb_max_val),
+        duration_seconds,
+    )
+    dual_pb_val = new_val
     return
 
 
