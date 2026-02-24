1da3ed87da8dbd27e9f218f51c83e6421eaf8946
huxiaoou <516984451@qq.com>
2026-02-24 23:16:47 +0800

    Refactor ability projectile/bowshoot/fireball with AbilityStatesMachine

--- scenes/abilities/ability_projectile.gd
+++ scenes/abilities/ability_projectile.gd
@@ -10,6 +10,7 @@ class_name AbilityProjectile
 
 
 func _ready() -> void:
+    super._ready()
     max_num_target_cells = 1
     max_num_target_units = 1
     return
@@ -19,24 +20,20 @@ func is_valid(cell: Vector2i) -> bool:
     return ManagerCellBattle.get_cell_occupiant(cell) != null and cell in available_cells
 
 
-func _process(_delta: float) -> void:
-    if not is_active:
+func process_aiming(_delta: float) -> void:
+    if target_cells.size() >= max_num_target_cells:
         return
-    if not is_casting:
-        if target_cells.size() >= max_num_target_cells:
-            return
-        potential_target_cell_new = ManagerCellBattle.get_indicator_cell()
-        if potential_target_cell_new not in available_cells:
-            return
-        if potential_target_cell != potential_target_cell_new:
-            ManagerCellBattle.set_cell_potential(potential_target_cell)
-            potential_target_cell = potential_target_cell_new
-            ManagerCellBattle.set_cell_focused(potential_target_cell)
-            var start: Vector2 = owner_unit.global_position
-            var end: Vector2 = ManagerCellBattle.cell_to_point(potential_target_cell)
-            var control: Vector2 = Utils.cal_control_point_for_bezier(start, end, curve_scale)
-            comp_track_drawer.update_points(start, control, end)
+    potential_target_cell_new = ManagerCellBattle.get_indicator_cell()
+    if potential_target_cell_new not in available_cells:
         return
+    if potential_target_cell != potential_target_cell_new:
+        ManagerCellBattle.set_cell_potential(potential_target_cell)
+        potential_target_cell = potential_target_cell_new
+        ManagerCellBattle.set_cell_focused(potential_target_cell)
+        var start: Vector2 = owner_unit.global_position
+        var end: Vector2 = ManagerCellBattle.cell_to_point(potential_target_cell)
+        var control: Vector2 = Utils.cal_control_point_for_bezier(start, end, curve_scale)
+        comp_track_drawer.update_points(start, control, end)
     return
 
 
@@ -47,17 +44,15 @@ func on_ranged_projectile_launched() -> void:
     return
 
 
-func try_launch() -> bool:
-    if super.try_launch():
-        target_units.append(ManagerCellBattle.get_cell_occupiant(target_cell))
-        owner_unit.adjust_animation_direction_from_cell(target_cell)
-        owner_unit.ranged_projectile_launched.connect(on_ranged_projectile_launched)
-        owner_unit.update_hit_box()
-        owner_unit.play_animation("ranged_attack")
-        await owner_unit.anim_player.animation_finished
-        finish()
-        return true
-    return false
+func launch() -> void:
+    target_units.append(ManagerCellBattle.get_cell_occupiant(target_cell))
+    owner_unit.adjust_animation_direction_from_cell(target_cell)
+    owner_unit.ranged_projectile_launched.connect(on_ranged_projectile_launched)
+    owner_unit.update_hit_box()
+    owner_unit.play_animation("ranged_attack")
+    await owner_unit.anim_player.animation_finished
+    finish()
+    return
 
 
 func finish() -> void:
