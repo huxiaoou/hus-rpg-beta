e0fdc3ebd0833e90163ecd59b721d499f15a2d23
huxiaoou <516984451@qq.com>
2026-03-05 22:29:39 +0800

    Add toggle_test variable to CellIndicatorBattle and use it to conditionally print the entered cell. Also, add a new input action "ToggleTest" to toggle the value of toggle_test.

--- scenes/levels/cell_indicator_battle.gd
+++ scenes/levels/cell_indicator_battle.gd
@@ -3,6 +3,7 @@ extends Node2D
 class_name CellIndicatorBattle
 
 var indicator_cell: Vector2i = Vector2i.ZERO
+var toggle_test: bool = false
 
 
 func _process(_delta: float) -> void:
@@ -13,5 +14,13 @@ func _process(_delta: float) -> void:
         return
     indicator_cell = new_mouse_cell
     position = ManagerCellBattle.cell_to_point(indicator_cell)
-    print("Enter Cell %s" % indicator_cell)
+    if toggle_test:
+        print("Enter Cell %s" % indicator_cell)
+    return
+
+
+func _unhandled_input(event: InputEvent) -> void:
+    if event.is_action_pressed("toggle_test"):
+        toggle_test = !toggle_test
+        print("toggle_test: %s" % toggle_test)
     return
