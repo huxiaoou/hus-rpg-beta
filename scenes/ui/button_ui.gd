f6fe355da67762d618346dd5506b13d62b67333c
huxiaoou <516984451@qq.com>
2026-03-19 22:42:36 +0800

    Perfect UI logic for main_menu

--- scenes/ui/button_ui.gd
+++ scenes/ui/button_ui.gd
@@ -2,6 +2,31 @@ extends Button
 
 class_name ButtonUI
 
+var is_focused: bool = false
+
 
 func _on_mouse_entered() -> void:
     grab_focus()
+    is_focused = true
+    return
+
+
+func _input(event: InputEvent) -> void:
+    if event is not InputEventMouseMotion:
+        return
+
+    var mouse_pos: Vector2 = get_global_mouse_position()
+    if is_focused:
+        return
+    if mouse_pos.y > global_position.y + size.y:
+        return
+    if mouse_pos.y < global_position.y:
+        return
+    grab_focus()
+    is_focused = true
+    return
+
+
+func _on_focus_exited() -> void:
+    is_focused = false
+    return
