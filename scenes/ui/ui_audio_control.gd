41db58abe90c911e442df4fefca7808a5a7f1f81
huxiaoou <516984451@qq.com>
2026-03-27 22:01:00 +0800

    Remove some prints for UIAudioControl

--- scenes/ui/ui_audio_control.gd
+++ scenes/ui/ui_audio_control.gd
@@ -14,12 +14,12 @@ func _ready() -> void:
     label.text = bus_name
     bus_index = AudioServer.get_bus_index(bus_name)
     h_slider.value = AudioServer.get_bus_volume_linear(bus_index)
-    print("UIAudioControl ready for bus '%s' (index %d)" % [bus_name, bus_index])
-    print("Current volume: %f" % h_slider.value)
+    #print("UIAudioControl ready for bus '%s' (index %d)" % [bus_name, bus_index])
+    #print("Current volume: %f" % h_slider.value)
     return
 
 
 func _on_h_slider_value_changed(value: float) -> void:
     AudioServer.set_bus_volume_linear(bus_index, value)
-    print("Volume for bus '%s' (index %d) set to %f" % [bus_name, bus_index, value])
+    #print("Volume for bus '%s' (index %d) set to %f" % [bus_name, bus_index, value])
     return
