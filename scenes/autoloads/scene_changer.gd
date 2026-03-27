9973ee2e40b811a0f3aadb66ab5575abc67c2c5c
huxiaoou <516984451@qq.com>
2026-03-27 21:30:24 +0800

    Optimize logic for scene_changer

--- scenes/autoloads/scene_changer.gd
+++ scenes/autoloads/scene_changer.gd
@@ -1,34 +1,32 @@
 extends CanvasLayer
 
-@onready var color_rect: ColorRect = $ColorRect
+@onready var background: TextureRect = $Background
 @onready var progress_bar: ProgressBar = $ProgressBar
+@onready var label: Label = $Label
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var target_scene_path: String
-var is_ready: bool = false
+@onready var is_ready: bool = false
+@onready var allow_to_continue: bool = false
 
 
 func _ready() -> void:
     visible = false
+    label.modulate.a = 0
 
 
 func fade_in() -> void:
-    visible = true
-    color_rect.mouse_filter = Control.MOUSE_FILTER_STOP
     animation_player.play("fade_in")
     await animation_player.animation_finished
+    return
 
 
 func fade_out() -> void:
     animation_player.play("fade_out")
     await animation_player.animation_finished
-    color_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
-    visible = false
+    return
 
 
-func change_scene(_target_scene_path: String) -> void:
-    target_scene_path = _target_scene_path
-    is_ready = false
+func change_scene(target_scene_path: String) -> void:
     fade_in()
 
     var loader: Error = ResourceLoader.load_threaded_request(target_scene_path)
@@ -41,26 +39,31 @@ func change_scene(_target_scene_path: String) -> void:
         var status = ResourceLoader.load_threaded_get_status(target_scene_path, progress)
         progress_bar.value = progress[0] * 100
         if status == ResourceLoader.THREAD_LOAD_LOADED:
+            animation_player.play("show_tips")
             is_ready = true
             break
         if status == ResourceLoader.THREAD_LOAD_FAILED or status == ResourceLoader.THREAD_LOAD_INVALID_RESOURCE:
             push_error("Error during load %s" % target_scene_path)
-        await get_tree().process_frame
+        # await get_tree().process_frame
+        await get_tree().create_timer(0.5).timeout
 
     await _wait_for_input()
     var new_packed_scene: PackedScene = ResourceLoader.load_threaded_get(target_scene_path)
     get_tree().change_scene_to_packed(new_packed_scene)
     fade_out()
+    allow_to_continue = false
+    is_ready = false
     return
 
 
-func _wait_for_input():
-    # Wait one frame to ensure previous clicks (that triggered the scene change)
-    # don't accidentally trigger this too early.
-    #await get_tree().process_frame
+func _unhandled_input(event: InputEvent) -> void:
+    if event.is_action_pressed("continue") and is_ready:
+        allow_to_continue = true
+        get_viewport().set_input_as_handled()
 
+
+func _wait_for_input():
     while true:
         await get_tree().process_frame
-        var event: InputEvent = await color_rect.gui_input
-        if event is InputEventMouseButton:
+        if allow_to_continue:
             return
